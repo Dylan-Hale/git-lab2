1e2dcd110bdab7b8988f5f46ee736147bea4b82b
Dylan Hale <dh394722@ohio.edu>
2024-09-04 17:48:00 -0400

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Dylan Hale>
+ *        Date: <9/4/2024>
+ * Description: produces the string
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
